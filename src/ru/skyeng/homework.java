049c53ee99fd177f6390d76efb0f6479fec262c8
ACER <snazarova0@>
2022-09-30 18:51:36 +0300

    первые 2 задания

--- src/ru/skyeng/homework.java
+++ src/ru/skyeng/homework.java
@@ -1,6 +1,32 @@
 package ru.skyeng;
 
 public class homework { public static void main(String[] args) {
+    System.out.println("Заданиея");
+    // task1
+
+    int clientOS = 0;
+    if (clientOS == 1) {
+        System.out.println("Установите приложение для Android версии по ссылке");
+    }
+    if (clientOS == 0) {
+        System.out.println("установите версию для IOS по сыллке");
+    }
+    // task2
+
+    int clientDeviceYear = 2015;
+    if (clientOS == 1 && clientDeviceYear > 2015) {
+        System.out.println("Скачайте скачайте приложение для Андройд по ссылке ");
+    } else if (clientOS == 0 && clientDeviceYear >2015) {
+        System.out.println("Скачайте приложение для IOS  поссылке");
+    } else if (clientOS == 1 && clientDeviceYear <= 2015) {
+        System.out.println("Скачайте облегченую версию для Андроид по ссылке");
+    } else if (clientOS == 0 && clientDeviceYear <= 2015) {
+        System.out.println("Скачайте облегченную версию для IOS  по ссылке");
+    }
+// task 3
+
+
+
 
 }
 }

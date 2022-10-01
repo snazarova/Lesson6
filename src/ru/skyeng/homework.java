9a37cd222ee65479dd99b17ebfd0933d4181a204
ACER <snazarova0@>
2022-10-01 16:10:55 +0300

    завершенное дз

--- src/ru/skyeng/homework.java
+++ src/ru/skyeng/homework.java
@@ -25,8 +25,51 @@ public class homework { public static void main(String[] args) {
     }
 // task 3
 
+   int year = 2022;
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        System.out.println(year + "год является високосным");
+    }else {
+        System.out.println(year + " год не является високосным ");
+    }
+// task 4
 
+    int deliveryDays = 1;
+    int deliveryDistance = 95;
+    if (deliveryDistance > 20) {
+        deliveryDays++;
+    }
+     if (deliveryDistance > 60) {
+         deliveryDays++;
+     }
+System.out.println("Потребуется дней " + deliveryDays);
 
+     //task 5
 
+      int monthNumber = 12;
+      switch (monthNumber) {
+          case 12:
+          case 1:
+          case 2:
+              System.out.println("Зима");
+              break;
+          case 3:
+          case 4:
+          case 5:
+              System.out.println("Весна");
+              break;
+          case 6:
+          case 7:
+          case 8:
+              System.out.println("Лето");
+              break;
+          case 9:
+          case 10:
+          case 11:
+              System.out.println("Осень");
+              break;
+          default:
+              System.out.println("Не корректное значение" + monthNumber);
+
+      }
 }
 }
